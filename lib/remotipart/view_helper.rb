7868c8edf0380441c7023a46e0648594b03ca1bd
Yael Avinun <yael.a@gammado.com>
2013-12-26 14:23:44 +0200

    add escape html to text area tag

--- lib/remotipart/view_helper.rb
+++ lib/remotipart/view_helper.rb
@@ -2,8 +2,12 @@ module Remotipart
   module ViewHelper
     def remotipart_response(options = {}, &block)
       content = with_output_buffer(&block)
-      response.content_type = Mime::HTML
-      text_area_tag('remotipart_response', String.new(content), options)
+      if remotipart_submitted?
+        response.content_type = Mime::HTML
+        text_area_tag('remotipart_response', String.new(html_escape(content)), options)
+      else
+        content
+      end
     end
   end
 end
