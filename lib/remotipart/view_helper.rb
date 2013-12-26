5a2f5715d54ccc52136e91d43a143d8ab1668070
Yael Avinun <yael.a@gammado.com>
2013-12-26 13:53:23 +0200

    remove conditional

--- lib/remotipart/view_helper.rb
+++ lib/remotipart/view_helper.rb
@@ -2,12 +2,8 @@ module Remotipart
   module ViewHelper
     def remotipart_response(options = {}, &block)
       content = with_output_buffer(&block)
-      if remotipart_submitted?
-        response.content_type = Mime::HTML
-        text_area_tag('remotipart_response', String.new(content), options)
-      else
-        content
-      end
+      response.content_type = Mime::HTML
+      text_area_tag('remotipart_response', String.new(content), options)
     end
   end
 end
